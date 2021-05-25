bf7838dc0cab91b6c7907ce7bec1f7579ecffc17
yunu <oa8859@gmail.com>
2021-05-26 07:46:20 +0900

    chapter01

--- src/com/javaex/ex03/Ex15.java
+++ src/com/javaex/ex03/Ex15.java
@@ -4,8 +4,20 @@ public class Ex15 {
 
 	public static void main(String[] args) {
 
-	
-
+		int i;
+		String s = "*";
+		
+		for(i=1; i<7; i++) {
+			System.out.println("******");
+		}
+		System.out.println();
+		
+		for(i=1; i<7; i++) {
+			
+			System.out.println(s);
+			s += "*";
+		}
+		System.out.println("천장이 빙글빙글 돈다 어지럽네");
 	}
 
 }

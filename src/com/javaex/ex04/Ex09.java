324d96dc61f8d4e37ba978b05ddea5d862d180da
yunu <oa8859@gmail.com>
2021-05-29 21:00:51 +0900

    chapter01

---
+++ src/com/javaex/ex04/Ex09.java
@@ -0,0 +1,16 @@
+package com.javaex.ex04;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=20; i++) {
+			if(i%2==0 || i%3==00) {
+				continue;
+			}
+			System.out.println(i);
+		}
+			
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex10.java
@@ -0,0 +1,16 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요.[0이면 종료]");
+		
+		sc.close();
+		
+	}
+
+}
